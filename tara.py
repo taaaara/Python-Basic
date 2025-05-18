7456e2f37b71a1265f8b3e6c7289002ebeb27049
taaaara <tlgummychuppa@gmail.com>
2025-05-18 02:47:52 +0000

    20250518

--- tara.py
+++ tara.py
@@ -5,6 +5,88 @@
 # https://docs.streamlit.io/
 
 import streamlit as st
+import base64
+
+# Function to convert an image file to a base64 encoded string.
+def get_base64_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+
+# Encode both images to base64
+image1_base64 = get_base64_image("1.png")
+image2_base64 = get_base64_image("2.png")
+
+
+# HTML code that swaps the image when the red box is clicked
+html_code = f"""
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    .container {{
+      position: relative;
+      display: inline-block;
+    }}
+    .clickable-area {{
+      position: absolute;
+      border: 2px solid red;
+      cursor: pointer;
+    }}
+    /* Style for the single box on 1.png */
+    #red-box {{
+      top: 100px;
+      left: 175px;
+      width: 235px;
+      height: 115px;
+    }}
+    /* Styles for the 7 boxes on 2.png */
+    .red-box-2 {{
+      display: none; /* Hidden initially */
+    }}
+    /* Example positions for 7 boxes on 2.png */
+    #box1 {{ top: 30px; left: 65px; width: 70px; height: 90px; }}
+    #box2 {{ top: 50px; left: 200px; width: 80px; height: 80px; }}
+    #box3 {{ top: 50px; left: 475px; width: 80px; height: 80px; }}
+    #box4 {{ top: 160px; left: 50px; width: 80px; height: 80px; }}
+    #box5 {{ top: 160px; left: 150px; width: 80px; height: 80px; }}
+    #box6 {{ top: 160px; left: 250px; width: 80px; height: 80px; }}
+    #box7 {{ top: 270px; left: 150px; width: 80px; height: 80px; }}
+  </style>
+</head>
+<body>
+  <div class="container">
+    <img id="main-image" src="data:image/png;base64,{image1_base64}" width="600" alt="Clickable Image">
+
+    <!-- Single box on 1.png -->
+    <div id="red-box" class="clickable-area" onclick="changeImage()"></div>
+
+    <!-- Seven boxes on 2.png, hidden initially -->
+    <div id="box1" class="clickable-area red-box-2" onclick="alert('Box 1 clicked!')"></div>
+    <div id="box2" class="clickable-area red-box-2" onclick="alert('Box 2 clicked!')"></div>
+    <div id="box3" class="clickable-area red-box-2" onclick="alert('Box 3 clicked!')"></div>
+    <div id="box4" class="clickable-area red-box-2" onclick="alert('Box 4 clicked!')"></div>
+    <div id="box5" class="clickable-area red-box-2" onclick="alert('Box 5 clicked!')"></div>
+    <div id="box6" class="clickable-area red-box-2" onclick="alert('Box 6 clicked!')"></div>
+    <div id="box7" class="clickable-area red-box-2" onclick="alert('Box 7 clicked!')"></div>
+  </div>
+
+  <script>
+    function changeImage() {{
+      // Change the image to 2.png
+      document.getElementById("main-image").src = "data:image/png;base64,{image2_base64}";
+      // Hide the single red box
+      document.getElementById("red-box").style.display = "none";
+      // Show all seven boxes
+      const boxes = document.getElementsByClassName('red-box-2');
+      for(let i=0; i < boxes.length; i++) {{
+        boxes[i].style.display = 'block';
+      }}
+    }}
+  </script>
+</body>
+</html>
+"""
+
 
 title = "No Hunger"      # "" for variable
 st.title(title)
@@ -17,8 +99,4 @@ st.write(text)
 st.write("")
 
 
-
-
-st.image("1.png")
-
-
+st.components.v1.html(html_code, height=700)
