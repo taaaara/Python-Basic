2ee4558af80599a34be73b5012cd30e96c2ba95b
taaaara <tlgummychuppa@gmail.com>
2025-05-25 02:58:47 +0000

    D20250525

--- tara.py
+++ tara.py
@@ -15,6 +15,7 @@ def get_base64_image(image_path):
 # Encode both images to base64
 image1_base64 = get_base64_image("1.png")
 image2_base64 = get_base64_image("2.png")
+image3_base64 = get_base64_image("3.png")
 
 
 # HTML code that swaps the image when the red box is clicked
@@ -32,60 +33,90 @@ html_code = f"""
       border: 2px solid red;
       cursor: pointer;
     }}
-    /* Style for the single box on 1.png */
     #red-box {{
       top: 100px;
       left: 175px;
       width: 235px;
       height: 115px;
     }}
-    /* Styles for the 7 boxes on 2.png */
     .red-box-2 {{
-      display: none; /* Hidden initially */
+      display: none;
     }}
-    /* Example positions for 7 boxes on 2.png */
     #box1 {{ top: 30px; left: 65px; width: 70px; height: 90px; }}
-    #box2 {{ top: 50px; left: 200px; width: 80px; height: 80px; }}
-    #box3 {{ top: 50px; left: 475px; width: 80px; height: 80px; }}
-    #box4 {{ top: 160px; left: 50px; width: 80px; height: 80px; }}
-    #box5 {{ top: 160px; left: 150px; width: 80px; height: 80px; }}
-    #box6 {{ top: 160px; left: 250px; width: 80px; height: 80px; }}
-    #box7 {{ top: 270px; left: 150px; width: 80px; height: 80px; }}
+    #box2 {{ top: 5px; left: 235px; width: 70px; height: 75px; }}
+    #box3 {{ top: 45px; left: 475px; width: 80px; height: 80px; }}
+    #box4 {{ top: 175px; left: 25px; width: 80px; height: 80px; }}
+    #box5 {{ top: 200px; left: 450px; width: 80px; height: 100px; }}
+    #box6 {{ top: 150px; left: 270px; width: 45px; height: 80px; }}
+    #box7 {{ top: 300px; left: 0px; width: 120px; height: 30px; }}
+    #thank-you-box {{
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      background-color: pink;
+      padding: 8px 12px;
+      font-weight: bold;
+      border-radius: 10px;
+      font-family: Arial, sans-serif;
+    }}
   </style>
 </head>
 <body>
   <div class="container">
     <img id="main-image" src="data:image/png;base64,{image1_base64}" width="600" alt="Clickable Image">
+    
+    <div id="thank-you-box" style="display: none;">
+      <span id="thank-you-points">Thank You Points: 0</span>
+    </div>
 
-    <!-- Single box on 1.png -->
     <div id="red-box" class="clickable-area" onclick="changeImage()"></div>
 
-    <!-- Seven boxes on 2.png, hidden initially -->
-    <div id="box1" class="clickable-area red-box-2" onclick="alert('Box 1 clicked!')"></div>
-    <div id="box2" class="clickable-area red-box-2" onclick="alert('Box 2 clicked!')"></div>
-    <div id="box3" class="clickable-area red-box-2" onclick="alert('Box 3 clicked!')"></div>
-    <div id="box4" class="clickable-area red-box-2" onclick="alert('Box 4 clicked!')"></div>
-    <div id="box5" class="clickable-area red-box-2" onclick="alert('Box 5 clicked!')"></div>
-    <div id="box6" class="clickable-area red-box-2" onclick="alert('Box 6 clicked!')"></div>
-    <div id="box7" class="clickable-area red-box-2" onclick="alert('Box 7 clicked!')"></div>
+    <div id="box1" class="clickable-area red-box-2" onclick="increasePoints('box1')"></div>
+    <div id="box2" class="clickable-area red-box-2" onclick="increasePoints('box2')"></div>
+    <div id="box3" class="clickable-area red-box-2" onclick="increasePoints('box3')"></div>
+    <div id="box4" class="clickable-area red-box-2" onclick="increasePoints('box4')"></div>
+    <div id="box5" class="clickable-area red-box-2" onclick="increasePoints('box5')"></div>
+    <div id="box6" class="clickable-area red-box-2" onclick="increasePoints('box6')"></div>
+    <div id="box7" class="clickable-area red-box-2" onclick="goToImage3()"></div>
   </div>
 
   <script>
+    let points = 0;
+    let breadClickedFirst = false;
+
     function changeImage() {{
-      // Change the image to 2.png
       document.getElementById("main-image").src = "data:image/png;base64,{image2_base64}";
-      // Hide the single red box
       document.getElementById("red-box").style.display = "none";
-      // Show all seven boxes
+
       const boxes = document.getElementsByClassName('red-box-2');
-      for(let i=0; i < boxes.length; i++) {{
+      for (let i = 0; i < boxes.length; i++) {{
         boxes[i].style.display = 'block';
       }}
+
+      document.getElementById("thank-you-box").style.display = "block";
+    }}
+
+    function increasePoints(boxId) {{
+      if (boxId === 'box2') {{
+        breadClickedFirst = true;
+        return;
+      }}
+      if (breadClickedFirst && boxId !== 'box7') {{
+        points++;
+        document.getElementById("thank-you-points").textContent = "Thank You Points: " + points;
+        breadClickedFirst = false;
+      }}
+    }}
+
+    function goToImage3() {{
+      document.getElementById("main-image").src = "data:image/png;base64,{image3_base64}";
     }}
   </script>
 </body>
 </html>
 """
+
+
 
 
 title = "No Hunger"      # "" for variable
