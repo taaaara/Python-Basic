fdb8d09ab2223ad0d743b8bab05c91ad07d1f324
taaaara <tlgummychuppa@gmail.com>
2025-06-01 02:44:16 +0000

    20250601

--- tara.py
+++ tara.py
@@ -15,7 +15,7 @@ def get_base64_image(image_path):
 # Encode both images to base64
 image1_base64 = get_base64_image("1.png")
 image2_base64 = get_base64_image("2.png")
-image3_base64 = get_base64_image("3.png")
+image4_base64 = get_base64_image("4.png")
 
 
 # HTML code that swaps the image when the red box is clicked
@@ -30,14 +30,9 @@ html_code = f"""
     }}
     .clickable-area {{
       position: absolute;
-      border: 2px solid red;
+      border: 2px solid transparent;  /* Invisible border */
+      background-color: transparent;  /* Transparent background */
       cursor: pointer;
-    }}
-    #red-box {{
-      top: 100px;
-      left: 175px;
-      width: 235px;
-      height: 115px;
     }}
     .red-box-2 {{
       display: none;
@@ -48,7 +43,15 @@ html_code = f"""
     #box4 {{ top: 175px; left: 25px; width: 80px; height: 80px; }}
     #box5 {{ top: 200px; left: 450px; width: 80px; height: 100px; }}
     #box6 {{ top: 150px; left: 270px; width: 45px; height: 80px; }}
-    #box7 {{ top: 300px; left: 0px; width: 120px; height: 30px; }}
+
+    .new-red-box {{
+      position: absolute;
+      border: 2px solid transparent;  /* Invisible border */
+      display: none;
+      background-color: transparent;
+      cursor: pointer;
+    }}
+
     #thank-you-box {{
       position: absolute;
       top: 10px;
@@ -62,14 +65,13 @@ html_code = f"""
   </style>
 </head>
 <body>
+
   <div class="container">
-    <img id="main-image" src="data:image/png;base64,{image1_base64}" width="600" alt="Clickable Image">
-    
+    <img id="main-image" src="data:image/png;base64,{image1_base64}" width="600" alt="Main Image" onclick="changeImage()">
+
     <div id="thank-you-box" style="display: none;">
       <span id="thank-you-points">Thank You Points: 0</span>
     </div>
-
-    <div id="red-box" class="clickable-area" onclick="changeImage()"></div>
 
     <div id="box1" class="clickable-area red-box-2" onclick="increasePoints('box1')"></div>
     <div id="box2" class="clickable-area red-box-2" onclick="increasePoints('box2')"></div>
@@ -77,47 +79,72 @@ html_code = f"""
     <div id="box4" class="clickable-area red-box-2" onclick="increasePoints('box4')"></div>
     <div id="box5" class="clickable-area red-box-2" onclick="increasePoints('box5')"></div>
     <div id="box6" class="clickable-area red-box-2" onclick="increasePoints('box6')"></div>
-    <div id="box7" class="clickable-area red-box-2" onclick="goToImage3()"></div>
   </div>
 
   <script>
     let points = 0;
-    let breadClickedFirst = false;
+    let breadClicked = false;
+    let currentImage = "1";
 
     function changeImage() {{
-      document.getElementById("main-image").src = "data:image/png;base64,{image2_base64}";
-      document.getElementById("red-box").style.display = "none";
+      if (currentImage === "1") {{
+        document.getElementById("main-image").src = "data:image/png;base64,{image2_base64}";
+        currentImage = "2";
 
-      const boxes = document.getElementsByClassName('red-box-2');
-      for (let i = 0; i < boxes.length; i++) {{
-        boxes[i].style.display = 'block';
+        let redBoxes = document.getElementsByClassName("red-box-2");
+        for (let i = 0; i < redBoxes.length; i++) {{
+          redBoxes[i].style.display = "block";
+        }}
+
+        document.getElementById("thank-you-box").style.display = "block";
       }}
-
-      document.getElementById("thank-you-box").style.display = "block";
     }}
 
     function increasePoints(boxId) {{
-      if (boxId === 'box2') {{
-        breadClickedFirst = true;
+      if (boxId === "box2") {{
+        breadClicked = true;
         return;
       }}
-      if (breadClickedFirst && boxId !== 'box7') {{
+
+      if (breadClicked && boxId !== "box7") {{
         points++;
         document.getElementById("thank-you-points").textContent = "Thank You Points: " + points;
-        breadClickedFirst = false;
+        breadClicked = false;
+
+        if (points === 20) {{
+          document.getElementById("main-image").src = "data:image/png;base64,{image4_base64}";
+          currentImage = "4";
+
+          let redBoxes = document.getElementsByClassName("red-box-2");
+          for (let i = 0; i < redBoxes.length; i++) {{
+            redBoxes[i].style.display = "none";
+          }}
+        }}
       }}
     }}
 
     function goToImage3() {{
-      document.getElementById("main-image").src = "data:image/png;base64,{image3_base64}";
+      document.getElementById("main-image").src = "data:image/png;base64,{image4_base64}";
+      currentImage = "4";
+
+      let redBoxes = document.getElementsByClassName("red-box-2");
+      for (let i = 0; i < redBoxes.length; i++) {{
+        redBoxes[i].style.display = "none";
+      }}
+
+      let newBoxes = document.getElementsByClassName("new-red-box");
+      for (let i = 0; i < newBoxes.length; i++) {{
+        newBoxes[i].style.display = "block";
+      }}
+    }}
+
+    function clickNewBox(boxId) {{
+      // Optional: behavior for boxes in 3.png
     }}
   </script>
 </body>
 </html>
 """
-
-
-
 
 title = "No Hunger"      # "" for variable
 st.title(title)
@@ -128,6 +155,5 @@ With that you can buy other foods which will make you more points.
 """
 st.write(text)
 st.write("")
-
 
 st.components.v1.html(html_code, height=700)
